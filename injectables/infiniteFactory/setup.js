30238166c8ad7d44cac5476034b495bf75160a76
Rookiehecker <94798292+Rookiehecker@users.noreply.github.com>
2022-02-10 08:20:35 -0500

    Delete injectables/infiniteFactory directory

--- injectables/infiniteFactory/setup.js
+++
@@ -1,7 +0,0 @@
-if (!blooketUtility.prefs.infiniteFactory) {
-  blooketUtility.prefs.infiniteFactory = {
-    title: "Infinite Factory",
-    description: "Prevent the timer from ticking in solo factory games",
-    value: true,
-  };
-}
